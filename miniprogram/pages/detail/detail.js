5ab258250f53073ad73e8beb8902d7e7570c32a5
y31 <525998520@qq.com>
2020-01-15 15:26:56 +0800

    授权功能相关

--- miniprogram/pages/detail/detail.js
+++ miniprogram/pages/detail/detail.js
@@ -13,68 +13,19 @@ Page({
     userInfo: {},
     voteId: "",
     openId: "",
+    hasUserInfo: false,
     beforeVote: true,
     enableTime: true,
   },
 
   onLoad(options) {
-    const {
-      voteId
-    } = options;
-    vote
-      .doc(voteId)
-      .get()
-      .then(data => {
-        console.log(data);
-        this.setData({
-          voteData: data.data,
-          swiperList: data.data.imgIdList,
-          voteId
-        });
-      });
-    wx.cloud
-      .callFunction({
-        name: "login"
-      })
-      .then(data => {
-        this.data.openId = data.result.openId;
-        this.confirmEnableTime();
-        this.confirmUserInfo();
-        this.getRetio();
-      });
-  },
-
-  bindGetUserInfo: function (e) {
-    var self = this;
-    if (e.detail.userInfo) {
-      //用户按了允许授权按钮
-      let userInfo = e.detail.userInfo;
-      // 获取到用户的信息了，打印到控制台上看下
-      console.log("用户的信息如下：");
-      console.log(e.detail.userInfo);
-      console.log(this.data.openId);
-      self.setData({
-        userInfo
-      });
-      //授权成功后,通过改变 isHide 的值，让实现页面显示出来，把授权页面隐藏起来
-      self.setData({
-        isHide: false
-      });
-    } else {
-      //用户按了拒绝按钮
-      wx.showModal({
-        title: "警告",
-        content: "您点击了拒绝授权，将无法进入小程序，请授权之后再进入!!!",
-        showCancel: false,
-        confirmText: "返回授权",
-        success: function (res) {
-          // 用户没有授权成功，不需要改变 isHide 的值
-          if (res.confirm) {
-            console.log("用户点击了“返回授权”");
-          }
-        }
-      });
-    }
+    this.getVoteData(options.voteId);
+    this.getOpenId().then(() => {
+      this.confirmEnableTime();
+      this.confirmUserInfo();
+      this.getRetio();
+    });
+    this.bindUserInfo();
   },
 
   // 轮播
@@ -84,10 +35,67 @@ Page({
     });
   },
 
+
+  // 绑定用户信息
+  bindUserInfo() {
+    const self = this;
+    wx.getUserInfo({
+      success: function (res) {
+        self.setData({
+          userInfo: res.userInfo,
+          hasUserInfo: true
+        })
+      }
+    })
+    console.log('bindUserInfo');
+  },
+
+  // 用户授权
+  getUserInfo(e) {
+    if (e.detail.userInfo) {
+      app.globalData.userInfo = e.detail.userInfo;
+      this.setData({
+        userInfo: e.detail.userInfo,
+        hasUserInfo: true
+      })
+      console.log('getUserInfo');
+      console.log(this.data.userInfo);
+    } else {
+      wx.showModal({
+        title: '警告',
+        content: '您点击了拒绝授权，将无法进入小程序，请授权之后再进入',
+        showCancel: false,
+        confirmText: '返回授权',
+      })
+    }
+  },
+
+  // 获取投票数据
+  getVoteData: async function (voteId) {
+    const res = await vote.doc(voteId).get();
+    this.setData({
+      voteData: res.data,
+      swiperList: res.data.imgIdList,
+      voteId
+    });
+    console.log('getVoteData');
+  },
+
+  // 获取用户openId
+  getOpenId: async function () {
+    const res = await wx.cloud.callFunction({
+      name: 'login'
+    });
+    this.setData({
+      openId: res.result.openId
+    })
+    console.log('getOpenId');
+    return new Promise((res) => res());
+  },
+
   // 响应投票
   handleVote: async function (data) {
-    // this指向可能有错
-    const self = this;
+    // const self = this;
     this.submitVote(data);
     // await info.add({
     //   data: self.data.userInfo
@@ -96,12 +104,10 @@ Page({
     this.setData({
       beforeVote: false
     });
-    console.log("setData");
   },
 
   // 获取选项占比
   getRetio: async function () {
-    // return new Promise(reslove => {
     const data = await wx.cloud.callFunction({
       name: "ratio",
       data: {
@@ -111,9 +117,7 @@ Page({
     this.setData({
       ratioList: data.result.ratioList
     });
-    console.log(data);
-    // reslove()
-    // })
+    console.log('getRetio');
   },
 
   // 提交投票
@@ -126,7 +130,6 @@ Page({
         index: data.currentTarget.dataset.index
       }
     });
-    console.log("submitVote");
   },
 
   // 确认用户信息
@@ -141,6 +144,7 @@ Page({
     this.setData({
       beforeVote: res.result
     });
+    console.log('confirmUserInfo');
   },
 
   // 确认生效状态
@@ -154,5 +158,6 @@ Page({
     this.setData({
       enableTime: res.result
     })
+    console.log('confirmEnableTime');
   },
 });
