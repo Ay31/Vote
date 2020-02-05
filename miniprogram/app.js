c751c68b1cb0c52184baa491d8670206ecc8b301
y31 <525998520@qq.com>
2020-02-05 23:51:02 +0800

    feat(app.js): 初始化登录从云开发替换至自建后台
    BREAKING CHANGE: 原有登录失效

--- miniprogram/app.js
+++ miniprogram/app.js
@@ -41,8 +41,36 @@ App({
     })
   },
 
+  // userAuthCb: function() {
+  //   let that = this
+  //   wx.showLoading({
+  //     title: '加载中'
+  //   })
+  //   wx.getUserInfo({
+  //     success: function(res) {
+  //       that.globalData.userInfo = res.userInfo
+  //       that.globalData.encryptedData = res.encryptedData
+  //       that.globalData.iv = res.iv
+  //       console.log(res.userInfo)
+  //       console.log(res.code);
+  //       if (res.encryptedData &&  res.iv) {
+  //         wx.login({
+  //           success: function(res) {
+  //             if (res.code) {
+  //               console.log(res.code)
+  //               info.add({
+  //                 data: that.globalData.userInfo
+  //               })
+  //             }
+  //           }
+  //         })
+  //       }
+  //     }
+  //   })
+  // }
   userAuthCb: function() {
     let that = this
+    // let baseUrl = config.getBaseUrl
     wx.showLoading({
       title: '加载中'
     })
@@ -51,14 +79,45 @@ App({
         that.globalData.userInfo = res.userInfo
         that.globalData.encryptedData = res.encryptedData
         that.globalData.iv = res.iv
-        console.log(res.userInfo)
         if (res.encryptedData && res.iv) {
           wx.login({
             success: function(res) {
               if (res.code) {
-                console.log('ok')
-                info.add({
-                  data: that.globalData.userInfo
+                console.log(res.code);
+                //将用户基本信息回传给服务器，并获取assess_token
+                wx.request({
+                  // url: baseUrl + '/auth/api/token',
+                  url: 'http://localhost:8080' + '/api/token',
+                  method: 'POST',
+                  data: {
+                    code: res.code,
+                    // encryptedData: that.globalData.encryptedData,
+                    // iv: that.globalData.iv
+                  },
+                  header: {
+                    accept: 'application/json'
+                  },
+                  success: function(res) {
+                    console.log(res);
+                    let authorizationValue = res.data.access_token
+                    let currentPagesLen = getCurrentPages().length
+                    if (authorizationValue) {
+                      wx.setStorageSync('access_token', authorizationValue)
+                      wx.setStorageSync(
+                        'authorization',
+                        'Bearer ' + authorizationValue
+                      )
+                      that.globalData.userAuthorization =
+                        'Bearer ' + authorizationValue
+                      //判断是否有页面优先生成，如果生成则重新加载一次
+                      if (currentPagesLen !== 0) {
+                        wx.hideLoading()
+                        getCurrentPages()[currentPagesLen - 1].onLoad()
+                      }
+                    } else {
+                      console.log('身份验证失败')
+                    }
+                  }
                 })
               }
             }
