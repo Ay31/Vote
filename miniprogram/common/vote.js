3d64e69c30edd00aabbd1df0d2f1d0b4bcb056af
y31 <525998520@qq.com>
2020-02-17 06:08:55 +0800

    feat(info.js): info页面接口更换

--- miniprogram/common/vote.js
+++ miniprogram/common/vote.js
@@ -1,27 +1,33 @@
 import { postRequest, getRequest } from './request'
 import { baseUrl } from '../config'
 
-const testVote = () => {
+export const testVote = () => {
   getRequest(`${baseUrl}/api/vote/test`)
 }
 
 // 创建投票
-const createVote = data => {
+export const createVote = data => {
   return postRequest(`${baseUrl}/api/vote/createVote`, data)
 }
 
 // 获取投票详情
-const getVoteDetail = data => {
+export const getVoteDetail = data => {
   return getRequest(`${baseUrl}/api/vote/getVoteDetail?voteId=${data.voteId}`)
 }
 
 // 提交投票
-const submitVote = data => {
+export const submitVote = data => {
   return postRequest(`${baseUrl}/api/vote/submitVote`, data)
 }
 
 // 获取投票占比
-const getRetio = voteId => {
-  return postRequest(`${baseUrl}/api/vote/getRetio`, voteId)
+export const getRetio = data => {
+  return postRequest(`${baseUrl}/api/vote/getRetio`, data)
 }
-export { testVote, createVote, getVoteDetail, submitVote, getRetio }
+
+// 获取投票列表
+export const getVoteList = data => {
+  return getRequest(`${baseUrl}/api/vote/getVoteList?openId=${data.openId}`)
+}
+
+// export default *
--- miniprogram/pages/info/info.js
+++ miniprogram/pages/info/info.js
@@ -1,79 +1,49 @@
 // miniprogram/pages/info/info.js
+import { getVoteList } from '../../common/vote'
 
-const app = getApp();
-const db = wx.cloud.database();
-const vote = db.collection('vote');
+const app = getApp()
+const db = wx.cloud.database()
+const vote = db.collection('vote')
 
 Page({
   data: {
-    openId: '',
     voteData: {},
-    userInfo: {},
-    hasUserInfo: false
   },
 
-  onLoad: async function () {
-    await this.getOpenId();
-    this.getVoteList();
-  },
-
-  onShow() {
-    this.bindUserInfo();
-  },
-
-  // 绑定用户信息
-  bindUserInfo() {
-    const self = this;
-    wx.getUserInfo({
-      success: function (res) {
-        self.setData({
-          userInfo: res.userInfo,
-          hasUserInfo: true
-        })
-      }
-    })
-    console.log('bindUserInfo');
+  onLoad: async function() {
+    this.getVoteList()
   },
 
   // 获取投票列表
-  getVoteList: async function () {
-    const res = await vote.where({
-      _openid: this.data.openId
-    }).get();
-    this.setData({
-      voteData: res.data
-    });
-    console.log('getVoteList');
-  },
-
-  // 获取用户openId
-  getOpenId: async function () {
-    const res = await wx.cloud.callFunction({
-      name: 'login'
-    });
-    this.setData({
-      openId: res.result.openId
-    })
-    console.log('getOpenId');
-    return new Promise((res) => res());
+  getVoteList: async function() {
+    try {
+      const res = await getVoteList({
+        openId: app.globalData.openId,
+      })
+      this.setData({
+        voteData: res.data.voteList,
+      })
+    } catch (error) {
+      console.error(error)
+    }
+    console.log('getVoteList')
   },
 
   // 分享投票
   onShareAppMessage(res) {
-    console.log(res);
+    console.log(res)
     return {
       title: res.target.dataset.title,
       imageUrl: res.target.dataset.img[0],
       path: `/pages/detail/detail?voteId=${res.target.dataset.voteId}`,
-    };
+    }
   },
 
   // 跳转至投票页
   targetToDetail(res) {
-    console.log(res);
+    console.log(res)
     wx.navigateTo({
-      url: `/pages/detail/detail?voteId=${res.currentTarget.dataset.id}`
-    });
+      url: `/pages/detail/detail?voteId=${res.currentTarget.dataset.id}`,
+    })
   },
-
-});
+})
